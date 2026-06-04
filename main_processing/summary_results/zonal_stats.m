96c9d7d4bba76d2afa9b97d2150460ab7a07dd5c
Alexander Nguyen <a.nguyen@wustl.edu>
2026-06-04 06:44:40 -0500

    Update LandCover figure
    Increase font size, boxplot linewidth, and axes linewidth.

--- main_processing/summary_results/zonal_stats.m
+++ main_processing/summary_results/zonal_stats.m
@@ -24,21 +24,21 @@ EIC_data = EIC_data(:, [1 2 2 3 4 4 5]);
 DEM_data = DEM_data(:, [1 2 2 3 4 4 5]);
 slope_data = slope_data(:, [1 2 2 3 4 4 5]);
 
-labels = {'FWM: Carex Aquatillis', 'Wet Sedge', 'Tussock Tundra'}';
+labels = {'Carex Aquatillis', 'Wet Sedge', 'Tussock Tundra'}';
 
 %%
 
 boxfig = figure;
-tiledlayout(2,2)
+tiledlayout(2,2,"TileSpacing","tight")
 nexttile
 boxplot(E_data.', 'Whisker', inf);
 xticks(1:numel(labels))
 xticklabels(labels)
 ylabel('Seasonal Subsidence (cm)')
 ax = gca;
-ax.FontSize = 15;
+ax.FontSize = 17.5;
 ylim([0, 25])
-text(0.6, 23, 'A.)','FontSize',15,'FontWeight','bold')
+text(0.6, 23, 'A.)','FontSize',17.5,'FontWeight','bold')
 
 nexttile
 boxplot(EIC_data.', 'Whisker', inf);
@@ -46,9 +46,9 @@ xticks(1:numel(labels))
 xticklabels(labels)
 ylabel('%EIC')
 ax = gca;
-ax.FontSize = 15;
+ax.FontSize = 17.5;
 ylim([0, 130])
-text(0.6, 120, 'B.)','FontSize',15,'FontWeight','bold')
+text(0.6, 120, 'B.)','FontSize',17.5,'FontWeight','bold')
 
 nexttile
 boxplot(DEM_data.', 'Whisker', inf);
@@ -56,9 +56,9 @@ xticks(1:numel(labels))
 xticklabels(labels)
 ylabel('Elevation (m)')
 ax = gca;
-ax.FontSize = 15;
+ax.FontSize = 17.5;
 ylim([0, 15])
-text(0.6, 14, 'C.)','FontSize',15,'FontWeight','bold')
+text(0.6, 14, 'C.)','FontSize',17.5,'FontWeight','bold')
 
 nexttile
 boxplot(slope_data.', 'Whisker', inf);
@@ -66,8 +66,8 @@ xticks(1:numel(labels))
 xticklabels(labels)
 ylabel(['Slope (',char(176),')'])
 ax = gca;
-ax.FontSize = 15;
-text(0.6, 6.5, 'D.)','FontSize',15,'FontWeight','bold')
+ax.FontSize = 17.5;
+text(0.6, 6.5, 'D.)','FontSize',17.5,'FontWeight','bold')
 
 %%
 
@@ -93,15 +93,19 @@ DEM_data = DEM_data(:, [1 1 2 3 3 ]);
 slope_data = slope_data(:, [1 1 2 3 3 ]);
 
 boxfig2 = figure;
-tiledlayout(2,2)
+tiledlayout(2,2,'TileSpacing','compact')
 nexttile
 boxplot(E_data.', 'Whisker', 0);
 xticks(1:numel(labels))
 xticklabels(labels)
 ylabel('Seasonal Subsidence (cm)')
 ax = gca;
-ax.FontSize = 15;
-text(0.6, 7, 'A.)','FontSize',15,'FontWeight','bold')
+ax.FontSize = 17.5;
+ax.LineWidth = 1.5;
+text(0.6, 7, 'A.)','FontSize',17.5,'FontWeight','bold')
+
+bx1 = findobj(gca,'Tag','boxplot');
+set(bx1.Children,'LineWidth',1.5)
 
 nexttile
 boxplot(EIC_data.', 'Whisker', 0);
@@ -109,8 +113,12 @@ xticks(1:numel(labels))
 xticklabels(labels)
 ylabel('%EIC')
 ax = gca;
-ax.FontSize = 15;
-text(0.6, 20, 'B.)','FontSize',15,'FontWeight','bold')
+ax.FontSize = 17.5;
+ax.LineWidth = 1.5;
+text(0.6, 20, 'B.)','FontSize',17.5,'FontWeight','bold')
+
+bx1 = findobj(gca,'Tag','boxplot');
+set(bx1.Children,'LineWidth',1.5)
 
 nexttile
 boxplot(DEM_data.', 'Whisker', 0);
@@ -118,8 +126,12 @@ xticks(1:numel(labels))
 xticklabels(labels)
 ylabel('Elevation (m)')
 ax = gca;
-ax.FontSize = 15;
-text(0.6, 4.7, 'C.)','FontSize',15,'FontWeight','bold')
+ax.FontSize = 17.5;
+ax.LineWidth = 1.5;
+text(0.6, 4.7, 'C.)','FontSize',17.5,'FontWeight','bold')
+
+bx1 = findobj(gca,'Tag','boxplot');
+set(bx1.Children,'LineWidth',1.5)
 
 nexttile
 boxplot(slope_data.', 'Whisker', 0);
@@ -127,5 +139,9 @@ xticks(1:numel(labels))
 xticklabels(labels)
 ylabel(['Slope (',char(176),')'])
 ax = gca;
-ax.FontSize = 15;
-text(0.6, 0.45, 'D.)','FontSize',15,'FontWeight','bold')
+ax.FontSize = 17.5;
+ax.LineWidth = 1.5;
+text(0.6, 0.45, 'D.)','FontSize',17.5,'FontWeight','bold')
+
+bx1 = findobj(gca,'Tag','boxplot');
+set(bx1.Children,'LineWidth',1.5)
