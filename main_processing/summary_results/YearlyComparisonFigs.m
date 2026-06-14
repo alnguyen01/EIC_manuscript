405e82eae5f7eb59af15f93a9cb4c0ede63b1f92
Alexander Nguyen <a.nguyen@wustl.edu>
2026-06-14 06:12:07 -0500

    Figure updates
    Editing E_ts and adding some supplemental figs.

--- main_processing/summary_results/YearlyComparisonFigs.m
+++ main_processing/summary_results/YearlyComparisonFigs.m
@@ -156,3 +156,136 @@ title('')
 ax = gca;
 ax.FontSize = 15;
 set(ax,'Box', 'off')
+%% age-subsidence plots
+summary_age = readtable("results_stats.xlsx","Sheet","TL_Age");
+
+age = summary_age.Age_uncal;
+sub2019 = summary_age.Med_Subsidence_2019_cm;
+sub2021 = summary_age.Med_Subsidence_2021_cm;
+sub2019_uncert = summary_age.Mean_E_Uncert_2019;
+sub2021_uncert = summary_age.Mean_E_Uncert_2021;
+
+coastal_age = age([1:2,4:5,7],:);
+coastal_sub2019 = sub2019([1:2,4:5,7],:);
+coastal_sub2021 = sub2021([1:2,4:5,7],:);
+
+coastal_sub2019uncert = sub2019_uncert([1:2,4:5,7],:);
+coastal_sub2021uncert = sub2021_uncert([1:2,4:5,7],:);
+coastal_names = summary_age.Sample_basin_ID([1:2,4:5,7],:);
+
+inland_age = age([3,6,8],:);
+inland_sub2019 = sub2019([3,6,8],:);
+inland_sub2021 = sub2021([3,6,8],:);
+inland_sub2019uncert = sub2019_uncert([3,6,8],:);
+inland_sub2021uncert = sub2021_uncert([3,6,8],:);
+inland_names = summary_age.Sample_basin_ID([3,6,8],:);
+
+fitlincoastal2019 = fitlm(coastal_age,coastal_sub2019,'linear');
+fitlininland2019 = fitlm(inland_age,inland_sub2019,"linear");
+fitlincoastal2021 = fitlm(coastal_age,coastal_sub2021,'linear');
+fitlininland2021 = fitlm(inland_age,inland_sub2021,"linear");
+
+figure
+f3 = figure;
+tiledlayout(2,2)
+nexttile
+yline(6.7,'-k','14C DTLB Average = 6.7','Color','k','FontSize',15)
+hold on
+yline(5.3,'-r','Study Area-Wide Average = 5.3','Color','r','FontSize',15)
+pi9 = plot(fitlininland2019);
+% for i = 1:3
+%     text(inland_age(i)+10, inland_sub2019(i)+.25, inland_names(i), 'FontSize',15)
+% end
+pi9(1).MarkerSize = 12;
+pi9(1).Marker = 'o';
+pi9(1).MarkerFaceColor = 'b';
+errorbar(inland_age,inland_sub2019,inland_sub2019uncert,'LineStyle','none','Color','k')
+xlabel('Drainage Age', 'FontSize', 15);
+ylabel('Seasonal Subsidence (cm)')
+xlim([0, 2500])
+ylim([0, 12])
+legend(["14C DTLB Average", "Study Area-Wide Average", "Data", "Linear Fit", "95% conf.bounds"],'Location','southwest')
+text(10,10.5,['y = ',num2str(fitlininland2019.Coefficients.Estimate(2)),'x +',num2str(fitlininland2019.Coefficients.Estimate(1))],'FontSize',15)
+text(10,9.8,['$R^{2} =$',num2str(round(fitlininland2019.Rsquared.Ordinary,2))],'Interpreter','latex','fontsize',15)
+text(10, 11.5, 'A.)','FontSize',15,'FontWeight','bold')
+title('')
+ax = gca;
+ax.FontSize = 15;
+set(ax,'Box', 'off')
+
+nexttile
+yline(5.7,'-k','14C DTLB Average = 5.7','Color','k','FontSize',15)
+hold on
+yline(5.3,'-r')
+pc9 = plot(fitlincoastal2019);
+% for i = 1:5
+%     text(coastal_age(i)+10, coastal_sub2019(i)+.25, coastal_names(i), 'FontSize',15)
+% end
+pc9(1).MarkerSize = 12;
+pc9(1).Marker = 'o';
+pc9(1).MarkerFaceColor = 'b';
+errorbar(coastal_age,coastal_sub2019,coastal_sub2019uncert,'LineStyle','none','Color','k')
+xlabel('Drainage Age', 'FontSize', 15);
+ylabel('Seasonal Subsidence (cm)')
+xlim([0, 2500])
+ylim([0, 12])
+legend(["14C DTLB Average", "Study Area-Wide Average", "Data", "Linear Fit", "95% conf.bounds"],'Location','southeast')
+text(10,10.5,['y = ',num2str(fitlincoastal2019.Coefficients.Estimate(2)),'x +',num2str(fitlincoastal2019.Coefficients.Estimate(1))],'FontSize',15)
+text(10,9.8,['$R^{2} =$',num2str(round(fitlincoastal2019.Rsquared.Ordinary,2))],'Interpreter','latex','fontsize',15)
+text(10, 11.5, 'B.)','FontSize',15,'FontWeight','bold')
+title('')
+ax = gca;
+ax.FontSize = 15;
+set(ax,'Box', 'off')
+
+%2021
+nexttile
+yline(2.7,'-k','14C DTLB Average = 2.7','Color','k','FontSize',15)
+hold on
+yline(1.8,'-r','Study Area-Wide Average = 1.8','Color','r','FontSize',15)
+pi1 = plot(fitlininland2021);
+% for i = 1:3
+%     text(inland_age(i)+10, inland_sub2021(i)+.25, inland_names(i), 'FontSize',15)
+% end
+pi1(1).MarkerSize = 12;
+pi1(1).Marker = 'o';
+pi1(1).MarkerFaceColor = 'b';
+errorbar(inland_age,inland_sub2021,inland_sub2021uncert,'LineStyle','none','Color','k')
+xlabel('Drainage Age', 'FontSize', 15);
+ylabel('Seasonal Subsidence (cm)')
+xlim([0, 2500])
+ylim([0, 5])
+legend(["14C DTLB Average", "Study Area-Wide Average", "Data", "Linear Fit", "95% conf.bounds"],'Location','southwest')
+text(10,3.8,['y = ',num2str(fitlininland2021.Coefficients.Estimate(2)),'x +',num2str(fitlininland2021.Coefficients.Estimate(1))],'FontSize',15)
+text(10,3.5,['$R^{2} =$',num2str(round(fitlininland2021.Rsquared.Ordinary,2))],'Interpreter','latex','fontsize',15)
+text(10, 4.5, 'C.)','FontSize',15,'FontWeight','bold')
+title('')
+ax = gca;
+ax.FontSize = 15;
+set(ax,'Box', 'off')
+
+nexttile
+yline(1,'-k','14C DTLB Average = 1','Color','k','FontSize',15)
+hold on
+yline(1.8,'-r')
+pc1 = plot(fitlincoastal2021);
+% for i = 1:5
+%     text(coastal_age(i)+10, coastal_sub2021(i)+.25, coastal_names(i), 'FontSize',15)
+% end
+pc1(1).MarkerSize = 12;
+pc1(1).Marker = 'o';
+pc1(1).MarkerFaceColor = 'b';
+errorbar(coastal_age,coastal_sub2021,coastal_sub2021uncert,'LineStyle','none','Color','k')
+xlabel('Drainage Age', 'FontSize', 15);
+ylabel('Seasonal Subsidence (cm)')
+xlim([0, 2500])
+ylim([0, 5])
+legend(["14C DTLB Average", "Study Area-Wide Average", "Data", "Linear Fit", "95% conf.bounds"],'Location','northeast')
+text(10,3.8,['y = ',num2str(fitlincoastal2021.Coefficients.Estimate(2)),'x +',num2str(fitlincoastal2021.Coefficients.Estimate(1))],'FontSize',15)
+text(10,3.5,['$R^{2} =$',num2str(round(fitlincoastal2021.Rsquared.Ordinary,2))],'Interpreter','latex','fontsize',15)
+text(10, 4.5, 'D.)','FontSize',15,'FontWeight','bold')
+title('')
+ax = gca;
+ax.FontSize = 15;
+set(ax,'Box', 'off')
+
